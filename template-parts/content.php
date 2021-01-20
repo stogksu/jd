98efc8f293c9e9420cdb3dacc8c176d9ee88c4c7
Oksana <ksushencij87@gmail.com>
2021-01-20 02:16:39 +0200

    add comment counter

--- template-parts/content.php
+++ template-parts/content.php
@@ -40,35 +40,37 @@
 		}
 		?>
 			
+		<div class="entry-content">
+			<?php
+			$content = get_the_content();
+			echo is_singular() ? $content : mb_strimwidth($content, 0, 500, '...');	
+			
+			/* the_content(
+				sprintf(
+					wp_kses(
+						// translators: %s: Name of current post. Only visible to screen readers 
+						__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'jd' ),
+						array(
+							'span' => array(
+								'class' => array(),
+							),
+						)
+					),
+					wp_kses_post( get_the_title() )
+				)
+			);
+			*/
 
-
-	<div class="entry-content">
-		<?php
-		$content = get_the_content();
-		echo is_singular() ? $content : mb_strimwidth($content, 0, 500, '...');	
+			?>	
 		
-		/* the_content(
-			sprintf(
-				wp_kses(
-					// translators: %s: Name of current post. Only visible to screen readers 
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'jd' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				wp_kses_post( get_the_title() )
-			)
-		);
-		*/
-
-		?>	
-	
-	</div><!-- .entry-content -->
+		</div><!-- .entry-content -->
 		<?php if (!is_singular()) { ?>
 			<a href="<?php echo esc_url(get_permalink()); ?>" alt="" class="read-post">Read post</a>
+			<span class="comment-counter">
+				<a href="<?php echo esc_url(get_permalink()); ?>"><?php echo get_comments_number() ?></a>
+			</span>
 		<?php } ?>
+		
 	</div>
 	
 	
